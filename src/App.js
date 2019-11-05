432b4559ad377ec94f17e2191a2cb487ce3079dc
Kolyabel <krchan@yandex.ru>
2019-11-05 14:11:29 +0300

    work

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ class App extends React.Component {
 
     render() {
         const fiveCircle = [
-            {radius:20, colour:"green", pointX:50, pointY:50},
-            {radius:40, colour:"blue", pointX:115, pointY:60},
+            {radius:50, colour:"green", pointX:50, pointY:50},
+            {radius:25, colour:"blue", pointX:140, pointY:60},
             {radius:32, colour:"yellow", pointX:200, pointY:40},
-            {radius:42, colour:"red", pointX:290, pointY:50},
-            {radius:10, colour:"purple", pointX:350, pointY:45},
+            {radius:25, colour:"red", pointX:270, pointY:50},
+            {radius:50, colour:"purple", pointX:350, pointY:50},
         ]
 
+        let randomX = Math.floor(Math.random()*400)
+        let randomY = Math.floor(Math.random()*100)
+
         const fivePoint = [
-            {name:1, pointX:60, pointY:35},
-            {name:2, pointX:100, pointY:20},
-            {name:3, pointX:150, pointY:45},
-            {name:4, pointX:270, pointY:60},
-            {name:5, pointX:270, pointY:47},
+            {pointX:Math.floor(Math.random()*400), pointY:Math.floor(Math.random()*100)},
+            {pointX:Math.floor(Math.random()*400), pointY:Math.floor(Math.random()*100)},
+            {pointX:Math.floor(Math.random()*400), pointY:Math.floor(Math.random()*100)},
+            {pointX:Math.floor(Math.random()*400), pointY:Math.floor(Math.random()*100)},
+            {pointX:Math.floor(Math.random()*400), pointY:Math.floor(Math.random()*100)},
         ]
 
         for (let i = 0; i < fivePoint.length; i++) {
@@ -29,7 +32,7 @@ class App extends React.Component {
                     ((fivePoint[i].pointX - fiveCircle[j].pointX) ** 2 +
                         (fivePoint[i].pointY - fiveCircle[j].pointY) ** 2))) {
 
-                    console.log("точка", fivePoint[i].name, "в круге", fiveCircle[j].colour)
+                    console.log("точка", i + 1, "в круге", fiveCircle[j].colour)
                     break;
                 }
             }
