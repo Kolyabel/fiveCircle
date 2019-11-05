0a0a343fd0109b71758a4125c0c94ba842a1ab12
Kolyabel <krchan@yandex.ru>
2019-11-05 14:12:32 +0300

    work

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component {
     render() {
         const fiveCircle = [
             {radius:50, colour:"green", pointX:50, pointY:50},
-            {radius:25, colour:"blue", pointX:140, pointY:60},
+            {radius:28, colour:"blue", pointX:140, pointY:60},
             {radius:32, colour:"yellow", pointX:200, pointY:40},
-            {radius:25, colour:"red", pointX:270, pointY:50},
+            {radius:30, colour:"red", pointX:270, pointY:50},
             {radius:50, colour:"purple", pointX:350, pointY:50},
         ]
 
